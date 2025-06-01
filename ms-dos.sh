02d1ee47e6f331092db9d4f2a6eaeeffb6227b95
theernis <ernestas.pakstas@gmail.com>
2025-06-01 19:13:47 +0300

    moved file copying from dosbox-x shell to bash in ms-dos.sh

--- ms-dos.sh
+++ ms-dos.sh
@@ -10,6 +10,8 @@ ARGUMENTS=$@
 cd $PROJECT_PATH
 [[ -d "ms-dos" ]] || mkdir "ms-dos"
 mount -o loop,offset=32256,uid=$UID ms-dos.img ./ms-dos
+
+cp ./Tasm ./ms-dos/DOS
 cp ./ms-dos/AUTOEXEC.BAT ./ms-dos/AUTOEXEC.BAT.BKP
 
 sed -i '/@ECHO OFF/d' ./ms-dos/AUTOEXEC.BAT
@@ -20,7 +22,7 @@ printf "tasm $FILENAME\r\n" >> ./ms-dos/AUTOEXEC.BAT
 printf "tlink $FILENAME\r\n" >> ./ms-dos/AUTOEXEC.BAT
 printf "$FILENAME $ARGUMENTS\r\n" >> ./ms-dos/AUTOEXEC.BAT
 
-dosbox-x --noautoexec -c "imgmount 0 -fs none -t floppy empty" -c "imgmount C ms-dos.img -ide 1m" -c "mount d ./Tasm" -c "copy d:\\ c:\\dos\\" -c "mount d -u" -c "mount d ./Prog" -c "boot C:"
+dosbox-x --noautoexec -c "imgmount 0 -fs none -t floppy empty" -c "imgmount C ms-dos.img -ide 1m" -c "mount d ./Prog" -c "boot C:"
 
 cp ./ms-dos/AUTOEXEC.BAT.BKP ./ms-dos/AUTOEXEC.BAT
 rm ./ms-dos/AUTOEXEC.BAT.BKP
