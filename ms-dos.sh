1465c9389e5abbda4e654c28096bd2cd4b53292d
theernis <ernestas.pakstas@gmail.com>
2025-06-09 19:31:53 +0300

    tried fixing ms-dos.sh

--- ms-dos.sh
+++ ms-dos.sh
@@ -12,8 +12,8 @@ cd $PROJECT_PATH
 mount -o loop,offset=32256,uid=$UID ms-dos.img ./ms-dos
 
 cp ./Tasm/* ./ms-dos/DOS
-cp ./ms-dos/AUTOEXEC.BAT ./ms-dos/AUTOEXEC.BAT.BKP
-cp ./ms-dos/CONFIG.SYS ./ms-dos/CONFIG.SYS.BKP
+cp ./ms-dos/AUTOEXEC.BAT ./AUTOEXEC.BAT.BKP
+cp ./ms-dos/CONFIG.SYS ./CONFIG.SYS.BKP
 
 sed -i '/@ECHO OFF/d' ./ms-dos/AUTOEXEC.BAT
 printf "D:\r\n" >> ./ms-dos/AUTOEXEC.BAT
@@ -23,13 +23,14 @@ printf "tasm $FILENAME\r\n" >> ./ms-dos/AUTOEXEC.BAT
 printf "tlink $FILENAME\r\n" >> ./ms-dos/AUTOEXEC.BAT
 printf "$FILENAME $ARGUMENTS\r\n" >> ./ms-dos/AUTOEXEC.BAT
 
-printf "SHELL=C:\\DOS\\COMMAND.COM C:\\DOS /P /E:512\r\n" > ./ms-dos/CONFIG.SYS
-cat ./ms-dos/CONFIG.SYS.BKP >> ./ms-dos/CONFIG.SYS
+
+sed -i '1s/^/SHELL=C:\\DOS\\COMMAND.COM C:\\DOS \/P \/E:512\r\n/' ./ms-dos/CONFIG.SYS
+
 
 dosbox-x --noautoexec -c "imgmount 0 -fs none -t floppy empty" -c "imgmount C ms-dos.img -ide 1m" -c "mount d ./Prog" -c "boot C:"
 
-cp ./ms-dos/AUTOEXEC.BAT.BKP ./ms-dos/AUTOEXEC.BAT
-cp ./ms-dos/CONFIG.SYS.BKP ./ms-dos/CONFIG.SYS
-rm ./ms-dos/AUTOEXEC.BAT.BKP
-rm ./ms-dos/CONFIG.SYS.BKP
+cp ./AUTOEXEC.BAT.BKP ./ms-dos/AUTOEXEC.BAT
+cp ./CONFIG.SYS.BKP ./ms-dos/CONFIG.SYS
+rm ./AUTOEXEC.BAT.BKP
+rm ./CONFIG.SYS.BKP
 umount ./ms-dos
