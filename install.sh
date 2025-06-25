7666779a6bf90d358057a67336285c2efdc21231
theernis <ernestas.pakstas@gmail.com>
2025-06-25 12:32:11 +0300

    trying to fix auto execution in ms-dos

--- install.sh
+++ install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+
+case $(id -u) in
+	0 )	;;
+	* )	echo "this script needs root privileges, please run this script with sudo"; exit ;;
+esac
+
 dosbox-x -noautoexec -c 'IMGMAKE ms-dos.img -t hd -size 2048' -c 'IMGMOUNT C ms-dos.img' -c 'BOOT "Disk 1.img" "Disk 2.img" "Disk 3.img"'
+
+mount -o loop,offset=32256,uid=$UID ms-dos.img ./ms-dos
+sed -i '1s/^/SHELL=C:\\DOS\\COMMAND.COM C:\\DOS \/P \/E:512\r\n/' ./ms-dos/CONFIG.SYS
+unix2dos ./ms-dos/CONFIG.SYS
+umount ./ms-dos
--- ms-dos.sh
+++ ms-dos.sh
@@ -13,7 +13,6 @@ mount -o loop,offset=32256,uid=$UID ms-dos.img ./ms-dos
 
 cp ./Tasm/* ./ms-dos/DOS
 cp ./ms-dos/AUTOEXEC.BAT ./AUTOEXEC.BAT.BKP
-cp ./ms-dos/CONFIG.SYS ./CONFIG.SYS.BKP
 
 sed -i '/@ECHO OFF/d' ./ms-dos/AUTOEXEC.BAT
 printf "D:\r\n" >> ./ms-dos/AUTOEXEC.BAT
@@ -24,15 +23,9 @@ printf "tlink $FILENAME\r\n" >> ./ms-dos/AUTOEXEC.BAT
 printf "$FILENAME $ARGUMENTS\r\n" >> ./ms-dos/AUTOEXEC.BAT
 
 
-sed -i '1s/^/SHELL=C:\\DOS\\COMMAND.COM C:\\DOS \/P \/E:512\r\n/' ./ms-dos/CONFIG.SYS
-
-
 unix2dos ./ms-dos/AUTOEXEC.BAT
-unix2dos ./ms-dos/CONFIG.SYS
 dosbox-x --noautoexec -c "imgmount 0 -fs none -t floppy empty" -c "imgmount C ms-dos.img -ide 1m" -c "mount d ./Prog" -c "boot C:"
 
 cp ./AUTOEXEC.BAT.BKP ./ms-dos/AUTOEXEC.BAT
-cp ./CONFIG.SYS.BKP ./ms-dos/CONFIG.SYS
 rm ./AUTOEXEC.BAT.BKP
-rm ./CONFIG.SYS.BKP
 umount ./ms-dos
